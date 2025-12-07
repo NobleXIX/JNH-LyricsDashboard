861105f26c14ca9072f5d0125f2f5bf6eb15a801
NobleXIX <166650760+NobleXIX@users.noreply.github.com>
2025-12-07 17:53:24 -0500

    Update fetch_charts.R

--- R/fetch_charts.R
+++ R/fetch_charts.R
@@ -1,19 +1,22 @@
-library(rvest)
+library(readr)
 library(dplyr)
-library(stringr)
 
+# We'll use Spotify's Global daily chart CSV as our "chart" source
+# This replaces the previous Billboard + rvest scraping.
 fetch_billboard_hot100 <- function(chart_date = Sys.Date()) {
-  # Billboard uses URLs like https://www.billboard.com/charts/hot-100/2024-03-23/
-  url <- paste0("https://www.billboard.com/charts/hot-100/", chart_date, "/")
+  # Spotify global daily latest chart as CSV
+  url <- "https://spotifycharts.com/regional/global/daily/latest/download"
   
-  page <- read_html(url)
+  # The CSV has a header row and then data starting on line 2, so we skip 1
+  df <- readr::read_csv(url, skip = 1, show_col_types = FALSE)
+  # Typical columns: Position, Track Name, Artist, Streams, URL
   
-  songs <- page |> html_nodes(".o-chart-results-list-row-container")
-  
-  tibble(
-    rank = seq_along(songs),
-    track = songs |> html_nodes(".o-chart-results-list__item h3") |> html_text(trim = TRUE),
-    artist = songs |> html_nodes(".o-chart-results-list__item span") |> html_text(trim = TRUE),
-    chart_date = as.Date(chart_date)
-  )
+  df |>
+    transmute(
+      rank       = Position,
+      track      = `Track Name`,
+      artist     = Artist,
+      streams    = Streams,
+      chart_date = as.Date(chart_date)
+    )
 }
