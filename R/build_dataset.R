9a658a725e3a54aede5ae635a59ccdb7f09090ce
NobleXIX <166650760+NobleXIX@users.noreply.github.com>
2025-12-07 15:36:50 -0500

    Create build_dataset.R

---
+++ R/build_dataset.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(purrr)
+library(readr)
+library(DBI)
+library(RSQLite)
+
+source("R/fetch_charts.R")
+source("R/get_lyrics.R")
+source("R/clean_lyrics.R")
+
+build_dataset <- function() {
+
+  chart <- fetch_billboard_hot100(Sys.Date())
+
+  # Fetch lyrics
+  lyric_data <- map2_dfr(chart$artist, chart$track, get_lyrics)
+
+  # Clean + sentiment
+  scored <- clean_and_score_lyrics(lyric_data)
+
+  # Save CSV
+  write_csv(lyric_data, "data_raw/lyrics_raw.csv")
+  write_csv(chart, "data_raw/chart_raw.csv")
+  write_csv(scored$tidy_words, "data_clean/tidy_words.csv")
+  write_csv(scored$sentiment, "data_clean/sentiment.csv")
+  write_csv(scored$emotions, "data_clean/emotions.csv")
+
+  # Save to SQLite
+  con <- dbConnect(RSQLite::SQLite(), "data_db/lyrics.sqlite")
+  dbWriteTable(con, "lyrics", lyric_data, overwrite = TRUE)
+  dbWriteTable(con, "chart", chart, overwrite = TRUE)
+  dbWriteTable(con, "tidy_words", scored$tidy_words, overwrite = TRUE)
+  dbWriteTable(con, "sentiment", scored$sentiment, overwrite = TRUE)
+  dbWriteTable(con, "emotions", scored$emotions, overwrite = TRUE)
+  dbDisconnect(con)
+}
