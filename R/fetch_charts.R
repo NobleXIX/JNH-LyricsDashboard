4eb177a72bc277c2e745cb69b2ac883c8ad54c06
NobleXIX <166650760+NobleXIX@users.noreply.github.com>
2025-12-09 16:28:49 -0500

    Update fetch_charts.R

--- R/fetch_charts.R
+++ R/fetch_charts.R
@@ -1,22 +1,22 @@
 library(readr)
 library(dplyr)
 
-# We'll use Spotify's Global daily chart CSV as our "chart" source
-# This replaces the previous Billboard + rvest scraping.
+# Use Spotify's global daily chart CSV as the "chart" source
 fetch_billboard_hot100 <- function(chart_date = Sys.Date()) {
-  # Spotify global daily latest chart as CSV
   url <- "https://spotifycharts.com/regional/global/daily/latest/download"
   
-  # The CSV has a header row and then data starting on line 2, so we skip 1
+  # Skip the first line of description, then read the CSV
   df <- readr::read_csv(url, skip = 1, show_col_types = FALSE)
-  # Typical columns: Position, Track Name, Artist, Streams, URL
+  # We expect: column 1 = position, 2 = track name, 3 = artist, 4 = streams
   
-  df |>
-    transmute(
-      rank       = Position,
-      track      = `Track Name`,
-      artist     = Artist,
-      streams    = Streams,
-      chart_date = as.Date(chart_date)
-    )
+  # Just use the first 4 columns by index, regardless of their names
+  df <- df |> dplyr::select(1:4)
+  
+  tibble::tibble(
+    rank       = df[[1]],
+    track      = df[[2]],
+    artist     = df[[3]],
+    streams    = df[[4]],
+    chart_date = as.Date(chart_date)
+  )
 }
