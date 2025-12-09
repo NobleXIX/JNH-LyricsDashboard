65c1919d820d2986daca9a5cd76f4191a2a8aa61
NobleXIX <166650760+NobleXIX@users.noreply.github.com>
2025-12-09 16:04:59 -0500

    Update get_lyrics.R

--- R/get_lyrics.R
+++ R/get_lyrics.R
@@ -1,24 +1,29 @@
-library(httr2)
 library(jsonlite)
 library(dplyr)
 
 get_lyrics <- function(artist, track) {
   artist_enc <- URLencode(artist)
   track_enc  <- URLencode(track)
-  
+
   url <- paste0("https://api.lyrics.ovh/v1/", artist_enc, "/", track_enc)
-  
-  resp <- request(url) |> req_perform()
-  
-  if (resp$status_code != 200) {
-    return(tibble(artist, track, lyrics = NA))
+
+  # Use jsonlite directly, no httr2 needed
+  resp <- tryCatch(
+    jsonlite::fromJSON(url),
+    error = function(e) NULL
+  )
+
+  if (is.null(resp) || is.null(resp$lyrics)) {
+    return(tibble(
+      artist = artist,
+      track  = track,
+      lyrics = NA_character_
+    ))
   }
-  
-  json <- resp |> resp_body_json()
-  
+
   tibble(
     artist = artist,
-    track = track,
-    lyrics = json$lyrics
+    track  = track,
+    lyrics = resp$lyrics
   )
 }
