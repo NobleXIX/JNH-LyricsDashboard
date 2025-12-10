03879380a1cd02ac81ecf6d2e945523a89a0ac35
NobleXIX <166650760+NobleXIX@users.noreply.github.com>
2025-12-09 19:57:35 -0500

    Update fetch_charts.R

--- R/fetch_charts.R
+++ R/fetch_charts.R
@@ -1,11 +1,9 @@
 library(jsonlite)
 library(dplyr)
 
-# Use Apple iTunes Top Songs RSS (JSON) as "chart" source
 fetch_billboard_hot100 <- function(chart_date = Sys.Date()) {
   url <- "https://itunes.apple.com/us/rss/topsongs/limit=50/json"
   
-  # Get and flatten JSON
   json <- jsonlite::fromJSON(url, flatten = TRUE)
   entries <- json$feed$entry
   
@@ -13,7 +11,6 @@ fetch_billboard_hot100 <- function(chart_date = Sys.Date()) {
     stop("iTunes top songs feed returned no entries. Check the source format or URL.")
   }
   
-  # Be robust: don't rely on column names, just use the first two columns
   if (ncol(entries) < 2) {
     stop("iTunes top songs feed did not return at least 2 columns.")
   }
